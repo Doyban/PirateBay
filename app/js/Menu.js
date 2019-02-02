a4da57a93e33a7224193eabab4461bd4c6333e97
Daniel Danielecki <daniel.danielecki@mail.com>
2019-02-02 20:00:01 +0100

    Edit: Messenger API code

--- app/js/Menu.js
+++ app/js/Menu.js
@@ -18,6 +18,7 @@
     this.loginButton = this.game.add.button(this.game.world.width * 0.445, this.game.world.height * 0.3, "login", this.startLogin, this);
     this.shareButton = this.game.add.button(this.game.world.width * 0.7, this.game.world.height * 0.3, "share", this.startShare, this);
     this.playButton = this.game.add.button(this.game.world.width * 0.1, this.game.world.height * 0.5, "play", this.startPlay, this);
+    // TOOD: Put it back and integrate Messenger API.
     // this.storeButton = this.game.add.button(this.game.world.width * 0.6, this.game.world.height * 0.5, "store", this.startStore, this);
 
     localStorage.scoreRate = localStorage.scoreRate || 1; // Initialize scoreRate.
